763baa1120788738ed29a9c2a5380bd401361063
rSSi <rossi@klingt.org>
2018-09-14 11:45:48 +0200

    Second Commit - Getting things to work

---
+++ confirmation_email.php
@@ -0,0 +1,24 @@
+<?php
+$confirmation_mailbody = "<br>
+	Independent Space Index Vienna
+
+	<h3>ANMELDUNG ZUM NEWSLETTER</h3>
+
+	Jemand hat uns aufgefordert Ihre Emailadresse <[@email_to_add]> bei unserem Newsletter einzutragen.<br>
+	Wenn Sie von uns Informationen zugesandt bekommen möchten, klicken Sie bitte auf den untenstehenden Link und bestätigen Sie damit ihre Emailadresse!<br>
+	Sollten Sie keine Emails von <a href='https://independentspaceindex.at'>Independent Space Index Vienna</a> bekommen möchten, können Sie diese Nachricht einfach ignorieren!<br>
+	<br>
+	<br>
+
+	<h3>NEWSLETTER SIGNUP</h3>
+
+	Someone requested to add your email address <[@email_to_add]> to our newsletter!<br>
+	If this was not you and you do not wish to receive news from the <a href='https://independentspaceindex.at'>Independent Space Index Vienna</a>, please ignore this message!<br>
+<br>
+<br>
+<a href='[@confirmation_link]'><button> Confirm your email address </button></a>
+<br>
+<br>
+<br>";
+
+?>
